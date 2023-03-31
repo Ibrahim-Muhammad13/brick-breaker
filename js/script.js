def2b5fb4591696451b8c149422c136b5c289a58
Aya <aya.rashed.official@gmail.com>
2023-03-31 15:01:09 +0200

    draw the ball

--- js/script.js
+++ js/script.js
@@ -1,21 +1,47 @@
 const canvas =  /** @type {HTMLCanvasElement} */ (document.getElementById('myCanvas'));
 //the HTMLCanvasElement is a type cast to make vscode autocomplete works for canvas..it doesn't affect the code
 const ctx = canvas.getContext('2d');
-
+const BALL_RADIUS = 8;
 
 const canvasWidth = canvas.width=1500;
 const canvasHeight =canvas.height=900;
 const paddleWidth=150;
 const paddleHeight=10;
+const paddleMarginButton=100;
 
 let paddleX = canvasWidth/2 - paddleWidth/2;
+let paddleY = canvasHeight-paddleHeight-paddleMarginButton;
 //draw the paddle
 function drawPaddle(){
     ctx.beginPath();
     ctx.fillStyle= "#0095DD";
-    ctx.fillRect(paddleX,canvasHeight-paddleHeight-100,paddleWidth,paddleHeight);
+    ctx.fillRect(paddleX,paddleY,paddleWidth,paddleHeight);
     ctx.fill();
     ctx.closePath();
 }
 drawPaddle();
+
+//Create the ball
+const ball = {
+    x : canvasWidth/2,
+    y : paddleY - BALL_RADIUS,
+    radius : BALL_RADIUS,
+    speed : 4,
+    dx : 3,
+    dy : -3
+    
+}
+//Draw the ball
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    ctx.fillStyle = "#ffcd05";
+    ctx.fill();
+    
+    ctx.strokeStyle = "#2e3548";
+    ctx.stroke();
+    
+    ctx.closePath();
+}
+drawBall();
 
