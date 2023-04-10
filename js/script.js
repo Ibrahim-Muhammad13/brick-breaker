acba64b5c1be414f2dc0a277c53e89b82ab7a06d
Yehia Nabil <yehn@outlook.com>
2023-04-10 22:44:01 +0200

    added sounds

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 const canvas =  /** @type {HTMLCanvasElement} */ (document.getElementById('myCanvas'));
 //the HTMLCanvasElement is a type cast to make vscode autocomplete works for canvas..it doesn't affect the code
 const ctx = canvas.getContext('2d');
-const BALL_RADIUS = 8;
+const BALL_RADIUS = 10;
 
 const canvasWidth = canvas.width=window.innerWidth;
 const canvasHeight =canvas.height=window.innerHeight;
-const paddleWidth=150;
-const paddleHeight=10;
+const paddleWidth=300;
+const paddleHeight=20;
 const paddleMarginBottom=100;
 
 
 let paddleX = canvasWidth/2 - paddleWidth/2;
 let paddleY = canvasHeight-paddleHeight-paddleMarginBottom;
-let paddleDX=15;
+let paddleDX=20;
+
+//Sound effects
+let brickSfx = new Audio();
+brickSfx.src = "sounds/paddel.m4a"
+let paddelSfx = new Audio();
+paddelSfx.src = "sounds/brick.wav"
+let failSound = new Audio();
+failSound.src = "sounds/fail.wav"
+let winSound = new Audio();
+winSound.src = "sounds/win.wav"
+let wallSfx = new Audio();
+wallSfx.src = "sounds/brick.wav"
+
 //draw the paddle
 function drawPaddle(){
     ctx.beginPath();
@@ -31,7 +44,7 @@ const ball = {
     dy : -ballSpeed
     
 }
-console.log(ball.dx)
+
 //Draw the ball
 function drawBall(){
     ctx.beginPath();
@@ -48,15 +61,16 @@ function drawBall(){
 
 //bricks Shape
 const brick={
-    num_row : 4,
-    num_column : 4,
-    Brick_Width :150,
-    Brick_Height :50,
-    space_left :2,
-    space_top : 2,
-    margin_top :80 ,
-    margin_left :200 ,
-    color :"#0095DD"
+    num_row : 2,
+    num_column : 3,
+    Brick_Width :300,
+    Brick_Height :40,
+    space_left :5,
+    space_top : 5,
+    margin_top :200 ,
+    margin_left :850 ,
+  //   color :"#0095DD"
+ 
     
 }
 // create the Bricks
@@ -66,13 +80,16 @@ function create_Bricks(){
         bricks[rows]=[];
         for(let column = 0; column < brick.num_column; column++){
             bricks[rows][column]={
-                x: (column * (brick.space_left +brick.Brick_Width)+brick.space_left+brick.margin_left)/.5,
-                y: (rows * (brick.space_top + brick.Brick_Height) + brick.space_top +brick.margin_top)/.5,
-                status: 1
+                x: (column * (brick.space_left +brick.Brick_Width)+brick.space_left+brick.margin_left),
+                y: (rows * (brick.space_top + brick.Brick_Height) + brick.space_top +brick.margin_top),
+                status: 1,
+                color:"#0095DD",
+                hited:false
             }
         }
     }
 }
+create_Bricks();
 //Draw Bricks
 function draw_Bricks(){
     for(let rows = 0 ; rows < brick.num_row ; rows++){
@@ -80,7 +97,7 @@ function draw_Bricks(){
             let b=bricks[rows][column];
             if(b.status ==1){
                 ctx.beginPath();
-                ctx.fillStyle = brick.color;
+                ctx.fillStyle = b.color;
                 ctx.fillRect(b.x, b.y ,brick.Brick_Width, brick.Brick_Height)
                 ctx.fill();
                 ctx.closePath();
@@ -90,18 +107,33 @@ function draw_Bricks(){
 }
 
 // Bricks collision
-
 function bricksCollision() {
     for(let rows = 0 ; rows < brick.num_row ; rows++){
         for(let column = 0; column < brick.num_column; column++){
             let b=bricks[rows][column];
-            if(b.status ==1){
+            if(b.status){
                 if (ball.x + ball.radius > b.x &&
                     ball.x - ball.radius < b.x + brick.Brick_Width &&
                     ball.y + ball.radius > b.y &&
-                    ball.y + ball.radius < b.y + brick.Brick_Height){
+                    ball.y - ball.radius < b.y + brick.Brick_Height){
+                        if(!b.hited){
+                            b.color="red";
+                            b.hited=true;
+                        }else{
+                            score+=scoreIncrement;
+                            b.status = 0;
+                        }
+                        brickSfx.play()
                         ball.dy = -ball.dy;
-                        b.status = false;
+                        if(score==brick.num_row*brick.num_column*10){
+                            Level++;
+                            winSound.play()
+                            brick.num_row+=1;
+                            brick.num_column+=1;
+                            create_Bricks();
+                            resetBall();
+                            restPaddle();
+                        }
                     }
             }
         }
@@ -109,17 +141,18 @@ function bricksCollision() {
 }
 
 //Game Status Bar
-
+let score =0;
+let scoreIncrement=10;
 function Score() {
-  ctx.font = "1.5rem MatchupPro, sans-serif";
+  ctx.font = "3rem MatchupPro, sans-serif";
   ctx.fillStyle = "#0095DD";
-  ctx.fillText(`Score: 100`, canvasWidth/25, 25);
+  ctx.fillText(`Score: ${score}`, canvasWidth/25, 25);
 }
-
+let Level =1;
 function level() {
-  ctx.font = "1.5rem MatchupPro, sans-serif";
+  ctx.font = "3rem MatchupPro, sans-serif";
   ctx.fillStyle = "#0095DD";
-  ctx.fillText(`Level: 3`, (canvasWidth/2), 25);
+  ctx.fillText(`Level: ${Level}`, (canvasWidth/2), 25);
 }
 
 let life = 3;
@@ -136,11 +169,6 @@ function live() {
 
 }
 }
-//    ctx.drawImage(img, canvasWidth - 75, 10, 25, 25);
-//    ctx.drawImage(img, canvasWidth - 110, 10, 25, 25);
-//    ctx.drawImage(img, canvasWidth - 145, 10, 25, 25);
-//}
-//live()
 
 //move the paddle
 let moveRight=false;
@@ -186,6 +214,7 @@ let gameover= false;
 function showGameOVer(){
     document.getElementById('gameover').style.display='block';
     document.getElementById('lost').style.display='block';
+    document.getElementById('score').innerText+=score;
     document.getElementById('restart').addEventListener('click',function(){
         location.reload();
     })
@@ -209,8 +238,7 @@ function playAgain(){
     ctx.font = '40pt Kremlin Pro Web';
     ctx.fillStyle = 'red';
     ctx.fillText('Play Again', 1140,785);
-   document.addEventListener("click", resetBall);
-
+    document.addEventListener("click", resetBall());
 
 }
 
@@ -219,8 +247,8 @@ function playAgain(){
 function moveBall(){
     ball.x += ball.dx;
     ball.y += ball.dy;
-    dx = 3,
-    dy = -3
+    dx = 3;
+    dy = -3;
     
 }
 function stopBall(){
@@ -235,6 +263,7 @@ function stopBall(){
 function ballWallCollision(){
     if(ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0 ){
         ball.dx = - ball.dx;
+        wallSfx.play()
     }
     if(ball.y - ball.radius < 0 ){
         ball.dy = -ball.dy;
@@ -243,6 +272,7 @@ function ballWallCollision(){
     if(ball.y + ball.radius > canvas.height){
         //dy = -dy;
     life--;
+    failSound.play();
     live();
         resetBall();
         restPaddle();
@@ -270,11 +300,33 @@ paddleY = canvasHeight-paddleHeight-paddleMarginBottom;
 
 function paddleBallCollision(){
     if(ball.x<paddleX+paddleWidth&&ball.x>paddleX &&ball.y<paddleY+paddleHeight&&ball.y>paddleY){
-        ball.dx = -ballSpeed;
-        ball.dy= -ball.dy; 
+       paddelSfx.play();
+       let point = ball.x-(paddleX+paddleWidth/2);
+       point = point/(paddleWidth/2);
+       let angle = point*Math.PI/3;
+       ball.dx = ballSpeed*Math.sin(angle);
+       ball.dy= -ballSpeed*Math.cos(angle); 
+        // ball.dx = -ballSpeed;
+        // ball.dy= -ball.dy; 
     }
 }
 
+
+//move with mouse 
+document.addEventListener("mousemove", mouseMoveHandler);
+function mouseMoveHandler(e) {
+    const relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+      paddleX = relativeX - paddleWidth / 2;
+    }
+    if(paddleX+paddleWidth>canvasWidth){
+        paddleX=canvasWidth-paddleWidth;
+    }
+    if(paddleX<0){
+        paddleX=0;
+    }
+  }
+  
 
 //keep this function at the bottom
 function run(){
@@ -284,14 +336,12 @@ function run(){
     live();
     drawBall();
     drawPaddle();
+    draw_Bricks();
     movePaddle();
     moveBall();
     ballWallCollision();
-    create_Bricks();
-    draw_Bricks();
     bricksCollision()
     gameOver();
- 
     paddleBallCollision();
     if(!gameover){
         requestAnimationFrame(run);
